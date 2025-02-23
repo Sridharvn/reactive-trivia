4dde965b6850f6b345ca21099f48d38851b7c6f1
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-02-23 13:45:08 +0530

    Added quiz effects for loading questions with success and failure handling

---
+++ src/app/store/quiz/quiz.effects.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import * as QuizActions from './quiz.actions';
+import { QuizService } from '../../services/quiz.service';
+
+@Injectable()
+export class QuizEffects {
+  loadQuestions$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(QuizActions.loadQuestions),
+      switchMap((action) =>
+        this.quizService.getQuestions(action.questions).pipe(
+          map((response) =>
+            QuizActions.loadQuestionsSuccess({ questions: response.results })
+          ),
+          catchError((error) => of(QuizActions.loadQuestionsFailure({ error })))
+        )
+      )
+    )
+  );
+
+  constructor(private actions$: Actions, private quizService: QuizService) {}
+}
